91116c2d068f8c5598ab5eb2a464918217354230
cecastillo2001 <112583563+cecastillo2001@users.noreply.github.com>
2022-12-15 14:20:34 -0600

    Update grade.rb

--- app/models/grade.rb
+++ app/models/grade.rb
@@ -2,11 +2,15 @@ class Grade < ApplicationRecord
     validates_presence_of :student_id
     validates_presence_of :student_name
     validates_presence_of :student_grade
-    validate :grade_must_be_greater_than_or_equal_to_zero
+    validate :grade_must_be_between_0_and_100
+    validate :student_id_must_be_five_characters_long
+
+
+
 
     #custom validator
-    #makes sure grade is larger than zero
-    def grade_must_be_greater_than_or_equal_to_zero
+    #makes sure grade is larger than zero and less than 100
+    def grade_must_be_between_0_and_100
         #if it is not null, if it was null then we can't compare it to zero
         if student_grade != nil
             if student_grade < 0
@@ -17,4 +21,13 @@ class Grade < ApplicationRecord
             end
         end
     end
+
+    def student_id_must_be_five_characters_long
+        if student_id !=nil
+            if student_id.length !=5
+                errors.add(:student_id, "must be 5 characters long")
+            end
+        end
+    end
+
 end
