0b414727ef6e14d3989c7adb419885c47dcc4ebb
cecastillo2001 <112583563+cecastillo2001@users.noreply.github.com>
2022-12-15 21:15:50 -0600

    Update users.rb

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,15 @@ FactoryBot.define do
   factory :user do
       sequence(:email) { |n| "person-#{n}@example.com" }
       password { '123greetings' }
+
+
+      trait :teacher do
+        account_id { 1 }
+      end
+
+      trait :ta do
+        account_id { 0 }
+      end
+
   end
 end
