2b3e9784f2749848a637b6b6f929c4406792bfe9
cecastillo2001 <112583563+cecastillo2001@users.noreply.github.com>
2022-12-15 21:12:01 -0600

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,25 @@
 class User < ApplicationRecord
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
+
+  
+
+  def teacher?
+     
+    if account_id == 1
+    return true
+    end  
+  end
+
+  def ta?
+     
+    if account_id == 0
+    return true
+    end
+
+  end
+
 end
